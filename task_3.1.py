f18dc926d61c5061e898af7cb6ad5a06e6fe8d35
Gray <yram70@mail.ru>
2023-05-15 19:42:23 +0300

    изминен task_3.1.py

--- task_3.1.py
+++ task_3.1.py
@@ -24,4 +24,31 @@
 #   - новый класс не запрещено строить на базе существующих типов данных: списков, словарей и тд.
 #   - отображать в таблице/матрице название колонки не обязательно!
 #   - проявите фантазию :)
+# 
 
+# 1 вариант
+nbr_column = 10 
+grid = []  
+for i in range(nbr_column):
+    row = [] 
+    for j in range(nbr_column):
+        row.append(1)
+    grid.append(row)
+                
+""" to display your new table"""
+for i in grid:
+    print(i)
+
+#  2 Вариант 
+# nbr_co = int(input("number of columns : "))
+# nbr_li = int(input("number of lines: "))
+# grid = [[1]*nbr_co for i in range(nbr_li)]
+
+
+# def affiche(grid):
+#     for i in range(nbr_li):
+#         print(grid[i])
+
+# for i in range(nbr_co):
+#     grid[i][i] = 1
+# affiche(grid)
