a75ebf21df07dd7892d8c2e2f55c3740d09d63bf
Gray <yram70@mail.ru>
2023-05-31 19:56:26 +0300

    ДЗ изменен lvl_4/task_4.1.py

--- lvl_4/task_4.1.py
+++ lvl_4/task_4.1.py
@@ -21,53 +21,53 @@
 # ID школы:
 # Название школы:
 
-# connection = sqlite3.connect('teachers.db')
-# cursor = connection.cursor()
-# cqlquery = """CREATE TABLE Students (
-# Student_Id INTEGER NOT NULL PRIMARY KEY,
-# Student_Name TEXT NOT NULL,
-# School_id INTEGER NOT NULL
-# );"""
-# cursor.execute(cqlquery)
-# connection.commit()
-# connection.close()
+connection = sqlite3.connect('teachers.db')
+cursor = connection.cursor()
+cqlquery = """CREATE TABLE Students (
+Student_Id INTEGER NOT NULL PRIMARY KEY,
+Student_Name TEXT NOT NULL,
+School_id INTEGER NOT NULL
+);"""
+cursor.execute(cqlquery)
+connection.commit()
+connection.close()
 
-# connection = sqlite3.connect('teachers.db')
-# cursor = connection.cursor()
-# cqlquery = """INSERT INTO Students (Student_Id, Student_Name, School_id)
-# VALUES
-# ('201', 'Иван', 1),
-# ('202', 'Петр', 2),
-# ('203', 'Анастасия', 3),
-# ('204', 'Игорь', 4);"""
-# cursor.execute(cqlquery)
-# connection.commit()
-# connection.close()
-
-
-# import sqlite3
-# import pandas as pd
-
-# connection = sqlite3.connect("teachers.db")
-# query = "SELECT * FROM Students"
-# df = pd.read_sql(query,connection)
-# print(df)
-
-# df.to_excel('teachers.xlsx', index = False)
+connection = sqlite3.connect('teachers.db')
+cursor = connection.cursor()
+cqlquery = """INSERT INTO Students (Student_Id, Student_Name, School_id)
+VALUES
+('201', 'Иван', 1),
+('202', 'Петр', 2),
+('203', 'Анастасия', 3),
+('204', 'Игорь', 4);"""
+cursor.execute(cqlquery)
+connection.commit()
+connection.close()
 
 
-# import sqlite3
+import sqlite3
+import pandas as pd
 
-# def get_student(student_id):
-#   connection = sqlite3.connect("teachers.db")
-#   cursor = connection.cursor()
-#   query = "SELECT * FROM School JOIN Students ON School.School_id = Students.School_id WHERE Students.Student_id = ?"
-#   cursor.execute(query,(student_id,))
-#   records = cursor.fetchall()
-#   print(records)
-#   for rom in records:
-#     print()
+connection = sqlite3.connect("teachers.db")
+query = "SELECT * FROM Students"
+df = pd.read_sql(query,connection)
+print(df)
 
-# x = int(input("Введи ID:"))
+df.to_excel('teachers.xlsx', index = False)
 
-# get_student(x)
+
+import sqlite3
+
+def get_student(student_id):
+  connection = sqlite3.connect("teachers.db")
+  cursor = connection.cursor()
+  query = "SELECT * FROM School JOIN Students ON School.School_id = Students.School_id WHERE Students.Student_id = ?"
+  cursor.execute(query,(student_id,))
+  records = cursor.fetchall()
+  print(records)
+  for rom in records:
+    print()
+
+x = int(input("Введи ID:"))
+
+get_student(x)
